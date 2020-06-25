c5bf429a568aa67c32834ee6d9aaf15988ac59c9
joshualondono <joshualondono@outlook.com>
2020-06-25 15:40:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,28 @@
  ******************/
 
 
+function getFirstitemFrom(arr){
+  return arr[0]
+}
+
+function getLastitemFrom(arr){
+  return arr[arr.length -1]
+}
+
+function isLongList(arr){
+  return arr[3]
+}
+
+function firstItemNumber(arr){
+  if(arr.length <= 10){
+    return 'true'
+  }  
+}
+
+function secondCharThirdString(arr){
+  let name = arr[3];
+  return name[2]
+}  
 
 
 
